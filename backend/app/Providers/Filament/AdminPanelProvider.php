2f9d3d1c3c9de46298db3994e99dfdf0d25d7ea6
Horváth Dániel <horvathdaniel9701@gmail.com>
2026-05-16 10:29:18 +0200

    hu

--- backend/app/Providers/Filament/AdminPanelProvider.php
+++ backend/app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->locale('hu')
             ->colors([
                 'primary' => Color::Amber,
             ])
