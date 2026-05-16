09de66b546c6509be58817904e377f14b7434729
Horváth Dániel <horvathdaniel9701@gmail.com>
2026-05-16 10:21:31 +0200

    new config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,14 @@
-const createNextIntlPlugin = require("next-intl/plugin");
-
-const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
+const { PHASE_PRODUCTION_SERVER } = require("next/constants");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = withNextIntl(nextConfig);
+module.exports = (phase) => {
+  // next start fázisban a build már kész, SWC plugin nem kell
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    return nextConfig;
+  }
+  const createNextIntlPlugin = require("next-intl/plugin");
+  const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
+  return withNextIntl(nextConfig);
+};
